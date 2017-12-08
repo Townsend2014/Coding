5e606321af7d5dd841ace5f7836a2caccb583cd0
Townsend2014 <saymay.townsend@gmail.com>
2017-12-07 18:09:23 -0700

    Update comment section

--- State/main.js
+++ State/main.js
@@ -1,21 +1,21 @@
 
-      // Initial array of movies
+      // Initial array of animals
       var animals = ["Puppies", "Kittens", "Shark", "Lion"];
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      // displayAnimalInfo function re-renders the HTML to display the appropriate content
       function displayAnimalInfo() {
 
         var animal = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
-        // Creating an AJAX call for the specific movie button being clicked
+        // Creating an AJAX call for the specific animal button being clicked
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
 
-          // Creating a div to hold the movie
-          var animalDiv = $("<div class='movie'>");
+          // Creating a div to hold the animal
+          var animalDiv = $("<div class='animal'>");
 
           // Storing the rating data
           var rating = response.Rated;
@@ -35,26 +35,26 @@
           // Appending the image
           animalDiv.append(image);
 
-          // Putting the entire movie above the previous movies
+          // Putting the entire animal above the previous animals
           $("#animals-view").prepend(animalDiv);
         });
 
       }
 
-      // Function for displaying movie data
+      // Function for displaying animal data
       function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
+        // Deleting the animals prior to adding new animals
         // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
 
-        // Looping through the array of movies
+        // Looping through the array of animals
         for (var i = 0; i < animals.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
+          // Then dynamicaly generating buttons for each animal in the array
           // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var a = $("<button>");
-          // Adding a class of movie to our button
+          // Adding a class of animal to our button
           a.addClass("animal");
           // Adding a data-attribute
           a.attr("data-name", animals[i]);
@@ -65,20 +65,20 @@
         }
       }
 
-      // This function handles events where a movie button is clicked
+      // This function handles events where an animal button is clicked
       $("#add-animal").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var animal = $("#animal-input").val().trim();
 
-        // Adding movie from the textbox to our array
+        // Adding animal from the textbox to our array
         animals.push(animal);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of our animal array
         renderButtons();
       });
 
-      // Adding a click event listener to all elements with a class of "movie"
+      // Adding a click event listener to all elements with a class of "animal"
       $(document).on("click", ".animal", displayAnimalInfo);
 
       // Calling the renderButtons function to display the intial buttons
@@ -120,11 +120,11 @@
               // Creating an image tag
               var animalImage = $("<img>");
 
-              // Giving the image tag an src attribute of a proprty pulled off the
+              // Giving the image tag an src attribute of a property pulled off the
               // result item
               animalImage.attr("src", results[i].images.fixed_height.url);
 
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              // Appending the paragraph and animalImage we created to the "gifDiv" div we created
               gifDiv.append(p);
               gifDiv.append(animalImage);
 
